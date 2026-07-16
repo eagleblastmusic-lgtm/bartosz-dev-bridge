448484444d9912869cd551ac3fb482d08eacabd2
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-17 01:14:38 +0200

    feat: hand off idle completed sessions

--- bdb_bridge/scheduler.py
+++ bdb_bridge/scheduler.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .journal import Journal
 from .models import CommandRecord
+from .session_finalization import SessionFinalizer
 
 
 class SingleQueueScheduler:
@@ -9,4 +10,12 @@ class SingleQueueScheduler:
         self._journal = journal
 
     def claim_next(self) -> CommandRecord | None:
+        claimed = self._journal.claim_next_command()
+        if claimed is not None:
+            return claimed
+
+        handoff = SessionFinalizer(self._journal).handoff_ready_session()
+        if handoff is None:
+            return None
+
         return self._journal.claim_next_command()
