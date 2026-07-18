692c1404db522438a0c0339baa9f4f472b5958da
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-18 22:20:33 +0200

    feat: export P07 status and control contracts

--- bdb_gui/__init__.py
+++ bdb_gui/__init__.py
@@ -1,4 +1,12 @@
 from .bootstrap import BootstrapService
+from .operations import (
+    GUI_CONTROL_RESULT_SCHEMA,
+    GUI_PROJECT_STATUS_SCHEMA,
+    ControlAction,
+    ControlResult,
+    ProjectOperationsService,
+    ProjectStatusSnapshot,
+)
 from .state import (
     GUI_BOOTSTRAP_SCHEMA,
     GUI_PROJECT_SCHEMA,
@@ -8,8 +16,14 @@ from .state import (
 
 __all__ = [
     "GUI_BOOTSTRAP_SCHEMA",
+    "GUI_CONTROL_RESULT_SCHEMA",
     "GUI_PROJECT_SCHEMA",
+    "GUI_PROJECT_STATUS_SCHEMA",
     "BootstrapService",
     "BootstrapSnapshot",
+    "ControlAction",
+    "ControlResult",
     "GuiProject",
+    "ProjectOperationsService",
+    "ProjectStatusSnapshot",
 ]
