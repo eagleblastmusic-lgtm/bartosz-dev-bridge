d08ed26faf6de98a2be66f750ab843d13e82ef00
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-18 21:19:02 +0200

    feat: export the combined P04 Operator API

--- bdb_operator/__init__.py
+++ bdb_operator/__init__.py
@@ -1,12 +1,16 @@
-from .api import OperatorApi
 from .models import (
     OPERATOR_PROJECT_SCHEMA,
     OPERATOR_RESPONSE_SCHEMA,
     OperatorError,
     OperatorResponse,
 )
+from .observability import CURRENT_OPERATION_SCHEMA, EVENT_SCHEMA, LOG_SNAPSHOT_SCHEMA
+from .operator import OperatorApi
 
 __all__ = [
+    "CURRENT_OPERATION_SCHEMA",
+    "EVENT_SCHEMA",
+    "LOG_SNAPSHOT_SCHEMA",
     "OPERATOR_PROJECT_SCHEMA",
     "OPERATOR_RESPONSE_SCHEMA",
     "OperatorApi",
