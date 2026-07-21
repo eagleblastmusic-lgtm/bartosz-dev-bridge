4568ff9b23511084c658fe2332201baeddc8abc8
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-21 06:21:27 +0200

    refactor: keep Project Creator lazily imported from operator submodule

--- bdb_operator/__init__.py
+++ bdb_operator/__init__.py
@@ -6,14 +6,6 @@ from .models import (
 )
 from .observability import CURRENT_OPERATION_SCHEMA, EVENT_SCHEMA, LOG_SNAPSHOT_SCHEMA
 from .operator import OperatorApi
-from .project_creator import (
-    DEFAULT_ALLOWED_PATHS,
-    PROJECT_CREATOR_PLAN_SCHEMA,
-    PROJECT_CREATOR_RESULT_SCHEMA,
-    ProjectCreatorPlan,
-    ProjectCreatorResult,
-    ProjectCreatorService,
-)
 from .session_projection import (
     SESSION_ATTEMPT_SCHEMA,
     SESSION_HISTORY_SCHEMA,
@@ -22,7 +14,6 @@ from .session_projection import (
 
 __all__ = [
     "CURRENT_OPERATION_SCHEMA",
-    "DEFAULT_ALLOWED_PATHS",
     "EVENT_SCHEMA",
     "LOG_SNAPSHOT_SCHEMA",
     "SESSION_ATTEMPT_SCHEMA",
@@ -30,12 +21,7 @@ __all__ = [
     "SESSION_SUMMARY_SCHEMA",
     "OPERATOR_PROJECT_SCHEMA",
     "OPERATOR_RESPONSE_SCHEMA",
-    "PROJECT_CREATOR_PLAN_SCHEMA",
-    "PROJECT_CREATOR_RESULT_SCHEMA",
     "OperatorApi",
     "OperatorError",
     "OperatorResponse",
-    "ProjectCreatorPlan",
-    "ProjectCreatorResult",
-    "ProjectCreatorService",
 ]
