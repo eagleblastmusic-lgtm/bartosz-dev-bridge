00e02261ebde310ba803bf9a16961ca6bc0f84ce
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-21 11:15:23 +0200

    Install background startup diagnostics

--- bdb_bridge/__main__.py
+++ bdb_bridge/__main__.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from .cli import main
+from . import cli
+from .background_start_diagnostics import install_background_start_diagnostics
+
+install_background_start_diagnostics(cli)
 
 if __name__ == "__main__":
-    main()
+    cli.main()
