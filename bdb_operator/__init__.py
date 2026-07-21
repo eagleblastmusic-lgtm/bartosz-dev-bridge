e55125803c9002174753030c4f1022698c306389
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-07-21 06:10:58 +0200

    feat: export Project Creator operator boundary

--- bdb_operator/__init__.py
+++ bdb_operator/__init__.py
@@ -6,6 +6,14 @@ from .models import (
 )
 from .observability import CURRENT_OPERATION_SCHEMA, EVENT_SCHEMA, LOG_SNAPSHOT_SCHEMA
 from .operator import OperatorApi
+from .project_creator import (
+    DEFAULT_ALLOWED_PATHS,
+    PROJECT_CREATOR_PLAN_SCHEMA,
+    PROJECT_CREATOR_RESULT_SCHEMA,
+    ProjectCreatorPlan,
+    ProjectCreatorResult,
+    ProjectCreatorService,
+)
 from .session_projection import (
     SESSION_ATTEMPT_SCHEMA,
     SESSION_HISTORY_SCHEMA,
@@ -14,6 +22,7 @@ from .session_projection import (
 
 __all__ = [
     "CURRENT_OPERATION_SCHEMA",
+    "DEFAULT_ALLOWED_PATHS",
     "EVENT_SCHEMA",
     "LOG_SNAPSHOT_SCHEMA",
     "SESSION_ATTEMPT_SCHEMA",
@@ -21,7 +30,12 @@ __all__ = [
     "SESSION_SUMMARY_SCHEMA",
     "OPERATOR_PROJECT_SCHEMA",
     "OPERATOR_RESPONSE_SCHEMA",
+    "PROJECT_CREATOR_PLAN_SCHEMA",
+    "PROJECT_CREATOR_RESULT_SCHEMA",
     "OperatorApi",
     "OperatorError",
     "OperatorResponse",
+    "ProjectCreatorPlan",
+    "ProjectCreatorResult",
+    "ProjectCreatorService",
 ]
